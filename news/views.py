c70f2c8b730b19c287ce6a572f537c22da2ca6c8
Назар Прохоров <nazar-prokhorov@rambler.ru>
2022-11-12 20:34:38 +0700

    Формы заполнения

--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 ﻿from urllib.request import Request
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import News, Category
+from .forms import NewsForm
 
 def index(request):
 	news = News.objects.all()
@@ -21,6 +22,22 @@ def view_news(request, news_id):
 	news_item = get_object_or_404(News, pk=news_id)
 	return render(request, 'news/view_news.html', {"news_item": news_item})
 
+def add_news(request):
+	if request.method == 'POST':
+		form = NewsForm(request.POST)
+		if form.is_valid():
+			#print(form.cleaned_data)
+			#news = News.objects.create(**form.cleaned_data) если через формы, через модели не нужно
+			news = form.save()
+			return redirect(news)
+	else:
+		form = NewsForm()
+	return render(request, 'news/add_news.html', {'form': form})
+
+
+
+
+#http запрос
 #	res='<h1>Список новостей</h1>'
 #	for item in news:
 #		res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>'
